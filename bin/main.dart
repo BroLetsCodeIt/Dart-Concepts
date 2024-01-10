88c9a3d69f185a2d5d72c160c2c69d1fee76dd80
kamleshkaparvena <kamleshkaparvena3570@gmail.com>
2024-01-10 20:24:32 +0530

    dart

---
+++ bin/main.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: unused_local_variable, unused_import
+
+/*
+  This is multi line comment
+*/
+
+// void main(){
+// variable declare
+
+// __________ built in datatypes
+// _________ int , double , String  , bool , List  , Map , Set , Runes , Symbol
+
+
+//   int integerNumber =  90;
+//   double doubleNumber =  23323.23;
+//   bool isNumber = true;
+//   String stringName = 'adslkfjasd';
+//   List < int > listname = [34 ,3434];
+//   List <String> liststring = ['asldkf','asdf' , 'af'];
+//   Map <String , int> ages = {
+//     'Alice' : 23 ,
+//     'bob':3
+//   };
+
+//   Set<int> uniqueNumber = {1,2,3};
+//   Runes heart = Runes('\u3423');
+//   Symbol symbol = #exampleSymbol;
+
+//   var quantity  = true;
+
+//   print(quantity.runtimeType);  // bool
+// }
+
+void main() {
+  // Arithmetic
+
+  // var add = 34 + 34;
+  // var sub  = 44 - 2;
+  // var mul = 21 * 2;
+  // var div = 21 / 2 ;
+
+  // var rawString = r"asldf\sdf\sf\sf\sf";
+
+
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+-  Dart is a client optimized programming language for apps on mutliple platforms.
+- It is developed by google and is used to build mobile , desktop , server , and web applications.
+- Dart is an object-oriented , class-based ,garbage-collected language with C-style syntax.
+- Dart can compile to either native code or javascript. 
